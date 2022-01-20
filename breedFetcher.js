11ac775a8deebc30ed2cc06104caae2ee00323dd
knagayama76 <knagayama76>
2022-01-20 16:30:31 -0500

    breedFetcher implemented

---
+++ breedFetcher.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const request = require("request");
+
+const args = process.argv.slice(2);
+
+request(
+  `https://api.thecatapi.com/v1/breeds/search?q=${args}`,
+  (error, response, body) => {
+    if (error) {
+      console.log("REQUEST failed");
+      return;
+    }
+
+    const data = JSON.parse(body);
+    if (!data[0]) {
+      console.error(`The breed doesn't exist!`);
+      return;
+    }
+    console.log(data[0].description);
+  }
+);
